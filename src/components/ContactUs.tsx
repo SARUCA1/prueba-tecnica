7b53a8398732791986f920a32a39d26a6e30cc71
Santiago <sruizc@correo.url.edu.gt>
2025-05-11 17:01:48 -0600

    ContactUs form

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -12,6 +12,56 @@ export function ContactUs() {
             }
         >
             {/* contenido */}
+            <div className="hero-content flex flex-col items-center justify-start text-center pt-32">
+                <h1 className="font-primary text-[#F5A31F] md:text-1xl pb-13">
+                    CONTACT US
+                </h1>
+                <h2 className="text-white text-5xl md:text-5xl font-primary mb-4">
+                    Tech and Talent - Unleashed
+                </h2>
+            </div>
+
+            <div className="w-full max-w-4xl mx-auto px-4 mt-12">
+                <form className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                {/* Name */}
+                    <div className="flex flex-col">
+                      <label className="text-white mb-2">Name</label>
+                      <input
+                        type="text"
+                        placeholder="Ramiro Ortega"
+                        className="w-full p-4 bg-white rounded-lg placeholder-gray-400 text-black focus:outline-none"
+                      />
+                    </div>
+                
+                    {/* Email */}
+                    <div className="flex flex-col">
+                      <label className="text-white mb-2">Email Address</label>
+                      <input
+                        type="email"
+                        placeholder="email@email.com"
+                        className="w-full p-4 bg-white rounded-lg placeholder-gray-400 text-black focus:outline-none"
+                      />
+                    </div>
+                
+                    {/* Message */}
+                    <div className="flex flex-col md:col-span-2">
+                      <label className="text-white mb-2">Message</label>
+                      <textarea
+                        placeholder="Your Message"
+                        rows={6}
+                        className="w-full p-4 bg-white rounded-lg placeholder-gray-400 text-black focus:outline-none"
+                      />
+                    </div>
+                
+                    {/* Submit */}
+                    <button
+                      type="submit"
+                      className="w-75 bg-[#F5A31F] text-white font-primary py-3 rounded-lg hover:bg-orange-600 transition"
+                    >
+                      Send Message
+                    </button>
+                </form>
+            </div>
         </section>
     )
 }
